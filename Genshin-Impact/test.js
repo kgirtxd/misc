4f4f658378081c1282eec6e93413418afb343166
Konard <retributionx99@gmail.com>
2021-12-18 14:30:31 -0600

    delete all

--- Genshin-Impact/test.js
+++
@@ -1,4 +0,0 @@
-var data = "<p>This is 'myWindow'</p>";
-myWindow = window.open("data:text/html," + encodeURIComponent(data),
-                       "_blank", "width=200,height=100");
-myWindow.focus();
