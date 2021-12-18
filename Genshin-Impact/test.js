c9d3f0377f383b90208383043271cfbd55329e6e
Konard <retributionx99@gmail.com>
2021-12-18 13:38:59 -0600

    Create test.js

---
+++ Genshin-Impact/test.js
@@ -0,0 +1,3 @@
+var tab = window.open('about:blank', '_blank');
+tab.document.write("Hello Kon"); // where 'html' is a variable containing your HTML
+tab.document.close(); // to finish loading the page
